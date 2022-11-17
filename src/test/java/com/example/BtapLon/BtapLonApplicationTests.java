5c79f9866d9b9ac5c30f4ae870bafc5f70e70771
vinhvi <vithevinh123@gmail.com>
2022-11-17 21:38:23 +0700

    create project

---
+++ src/test/java/com/example/BtapLon/BtapLonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BtapLon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtapLonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
